e1488cff67ad5273bd0b4ceb2979bdd2f7444edc
officialpryz <afolabipraise43@gmail.com>
2024-02-06 11:27:54 -0800

    commit task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" contain a function that reads a file """
+
+def read_file(filename=""):
+       """ Function that reads from a file
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+       with open(filename, 'r', encoding="utf-8") as f:
+           read_data = f.read()
+           print(read_data, end='')
