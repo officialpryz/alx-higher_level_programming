397b3a2d573d31f051ecdd469effd18541afa815
officialpryz <afolabipraise43@gmail.com>
2024-04-09 06:51:32 -0700

    update task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    return list.reduce((count, current) => current === searchElemennt ? count + 1 : count, 0);
+      let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
 };
-
