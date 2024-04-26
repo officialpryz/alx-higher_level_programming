4744c8b70477323d98522abb12633710c4a1e0e3
officialpryz <afolabipraise43@gmail.com>
2024-04-26 14:39:33 -0700

    task8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A script tha:
+- takes in a letter
+- sends POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    req = requests.post("http://0.0.0.0:5000/search_user",
+                        data={'q': "" if len(argv) < 2 else argv[1]})
+    try:
+        json_dict = req.json()
+        if not json_dict:
+            print("No result")
+            exit()
+        jsonid = json_dict.get("id")
+        jsonname = json_dict.get("name")
+        print("[{}] {}".format(jsonid, jsonname))
+    except ValueError as e:
+        print("Not a valid JSON")
+        
